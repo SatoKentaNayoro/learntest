203c89aec31d52e23fd96b98e0092e68915a9191
apehole <apehole@163.com>
2021-02-05 20:41:57 +0800

    11

--- canclefunc/main.go
+++ canclefunc/main.go
@@ -1,6 +1,17 @@
 package main
 
-func main() {
-	go func() {}()
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+)
 
+func main() {
+	var sig = make(chan os.Signal, 2)
+	defer fmt.Println(1111)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+	select {
+	case <-sig:
+	}
 }
